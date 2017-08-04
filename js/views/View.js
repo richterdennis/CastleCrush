e76876315a51e8f4063021693ca08c8f2b987e66
Dennis Richter <richter.dennis@live.de>
2017-08-04 22:26:46 +0200

    Create View Class that manages the view

---
+++ js/views/View.js
@@ -0,0 +1,35 @@
+export default class View {
+	constructor() {
+		this.templateUrl = "";
+		this.template = null;
+
+		this._holder = document.createElement('div');
+	}
+
+	async init() {
+		if(this.template === null) {
+			this.template = await fetch(`${this.templateUrl}.html`)
+				.then(r => r.text());
+		}
+
+		this._holder.innerHTML = this.template;
+		this.registerListener();
+
+		return this;
+	}
+
+	registerListener() {
+		const eventTypes = ['click'];
+
+		eventTypes.forEach(eventType => {
+			this._holder.querySelectorAll(`[\\40 ${eventType}]`).forEach(element => {
+				const funcName = element.getAttribute('@' + eventType);
+				element.addEventListener(eventType, event => this[funcName](event));
+			});
+		});
+	}
+
+	show(target) {
+		this._holder.childNodes.forEach(target.appendChild.bind(target));
+	}
+}
