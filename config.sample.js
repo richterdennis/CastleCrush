371aa3e329fe05f60904e1c6a6b93e2fd414a530
Dennis Richter <richter.dennis@live.de>
2017-08-27 16:38:08 +0200

    Add config.sample.js and add config.js to gitignore

--- config.sample.js
+++ config.sample.js
@@ -1,5 +1,5 @@
 window.CastleCrush = window.CastleCrush || {};
 CastleCrush.CONFIG = {
-	'SERVER_START_ADDRESS': 'http://localhost/CastleCrush/dist/public/server/index.php',
+	'SERVER_START_ADDRESS': 'public/server/index.php',
 	'SOCKET_ADDRESS':       'ws://localhost:9194/'
 };
