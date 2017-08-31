559390ad899f8395b42ab2d322094971ee7d1278
Dennis Richter <richter.dennis@live.de>
2017-08-31 12:03:53 +0200

    Finish room view

--- js/views/RoomView.js
+++ js/views/RoomView.js
@@ -7,4 +7,22 @@ export default class RoomView extends View {
 		this.title = 'Room';
 		this.templateUrl = 'templates/room';
 	}
+
+  afterInit() {
+    const roomId = 'test';
+
+    [1, 2].forEach(playerId => {
+      const joinArea = this.find(`.join-area:nth-child(${playerId})`);
+      const clearCode = joinArea.querySelector('.clear-code');
+      clearCode.value = `${roomId}-${playerId}`
+    });
+  }
+
+  joinLeft() {
+    CastleCrush.ViewManager.load('game');
+  }
+
+  joinRight() {
+    CastleCrush.ViewManager.load('game');
+  }
 }
