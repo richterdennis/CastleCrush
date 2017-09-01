4b171d6b78e731905cdf6ed06061ac23b0907b60
Dennis Richter <richter.dennis@live.de>
2017-09-01 18:00:21 +0200

    Belongs to the last

--- js/helper.js
+++ js/helper.js
@@ -2,5 +2,10 @@ export default {
 	uuid: () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
 		let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
 		return v.toString(16);
+	}),
+
+	generateRand6: () => 'xxxxxx'.replace(/[x]/g, (c) => {
+		const r = Math.random() * 16 | 0;
+		return r.toString(16);
 	})
 }
