49ca0e7e28b2e2663d5fe4588e38958c30011113
Dennis Richter <richter.dennis@live.de>
2017-09-01 17:55:33 +0200

    Implement GameManager

---
+++ js/game/GameManager.js
@@ -0,0 +1,34 @@
+import { EVENTS } from '../EventManager';
+
+export default class GameManager {
+	constructor() {
+		this.state = 'initial';
+
+		this.roomid = null;
+		this.difficulty = -1;
+		this.autoRestart = null;
+		this.player1 = null;
+		this.player2 = null;
+
+		CastleCrush.EventManager.addEventListener(EVENTS.START_ROOM, (event) => {
+			this.roomid = event.roomid;
+			this.difficulty = event.difficulty;
+			this.autoRestart = event.autoRestart;
+
+			this.state = 'inroom';
+		});
+
+		CastleCrush.EventManager.addEventListener(EVENTS.JOIN_ROOM, (event) => {
+			this.roomid = event.roomid;
+
+			this.state = 'inroom';
+		});
+
+		CastleCrush.EventManager.addEventListener(EVENTS.START_GAME, (event) => {
+			this.player1 = event.player1;
+			this.player2 = event.player2;
+
+			this.state = 'ingame';
+		});
+	}
+}
